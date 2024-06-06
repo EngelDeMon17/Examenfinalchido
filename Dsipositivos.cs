efdf134ea72a11069c5cf92d3b7ea68c6b640377
Waldemar Dell Montenegro <123352509+EngelDeMon17@users.noreply.github.com>
2024-06-06 09:38:37 -0600

    Agregar archivos de proyecto.

---
+++ Dsipositivos.cs
@@ -0,0 +1,20 @@
+﻿namespace Examenfinalchido
+{
+    public class Dispositivo
+    {
+        string numeroSerie;
+        string marca;
+        string modelo;
+
+        public Dispositivo()
+        {
+            numeroSerie = string.Empty;
+            marca = string.Empty;
+            modelo = string.Empty;
+        }
+
+        public string NumeroSerie { get => numeroSerie; set => numeroSerie = value; }
+        public string Marca { get => marca; set => marca = value; }
+        public string Modelo { get => modelo; set => modelo = value; }
+    }
+}
---
+++ Laptop.cs
@@ -0,0 +1,18 @@
+﻿namespace Examenfinalchido
+{
+    public class Laptop : Dispositivo
+    {
+        string memoriaRam;
+        string almacenamiento;
+
+        public Laptop()
+        {
+            memoriaRam = string.Empty;
+            almacenamiento = string.Empty;
+
+        }
+
+        public string MemoriaRam { get => memoriaRam; set => memoriaRam = value; }
+        public string Almacenamiento { get => almacenamiento; set => almacenamiento = value; }
+    }
+}
